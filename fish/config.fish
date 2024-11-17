ba382836492967c8a7de88cda577f4f9a7b72162
Kevin Renskers <kevin@loopwerk.io>
2024-11-17 20:33:39 +0100

    I don't use pyenv any more

--- fish/config.fish
+++ fish/config.fish
@@ -8,11 +8,6 @@ set -x -g LANG en_US.UTF-8
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/bin
 
-# pyenv
-set -Ux PYENV_ROOT $HOME/.pyenv
-fish_add_path $PYENV_ROOT/bin
-pyenv init - | source
-
 # pnpm
 set -gx PNPM_HOME $HOME/Library/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
